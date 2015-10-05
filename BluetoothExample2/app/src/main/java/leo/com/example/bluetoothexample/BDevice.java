746770b96ef91bed72caf64792c3cc3d1a3cbfef
LeobasCordero <leocorderoguzman@gmail.com>
2015-10-05 11:37:59 -0400

    bluetoothExample

---
+++ BluetoothExample2/app/src/main/java/leo/com/example/bluetoothexample/BDevice.java
@@ -0,0 +1,29 @@
+package leo.com.example.bluetoothexample;
+
+/**
+ * Created by Leobas on 04/10/2015.
+ */
+public class BDevice {
+
+    private String name;
+    private String address;
+
+    public BDevice() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
